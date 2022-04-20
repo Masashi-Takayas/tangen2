515f345e2b5b3f172f94fa8f6eb12abfaaab7a81
Masashi-Takayas <takayasu_masashi@axiz.co.jp>
2022-04-20 17:40:41 +0900

    complete

--- db_task_ddl.sql
+++ db_task_ddl.sql
@@ -1,15 +1,12 @@
 create table customer(
 customer_id int primary key
-,staff_name int
-);
+,staff_id int
+,address varchar(40)
+);	
 
 create table sale(
 sale_id int primary key,
-number int,
-customer_id int,
-constraint fk_customer
-foreign key(customer_id)
-references customer(customer_id)
+customer_id int
 );
 
 create table sale_detail(
@@ -17,20 +14,14 @@ sale_id int,
 sale_detail_id int,
 products_id int,
 buy_count int,
-primary key (sale_id,sale_detail_id),
-constraint fk_products
-foreign key(products_id)
-references products(products_id)
+primary key (sale_id,sale_detail_id)
 );
 
 create table products(
 products_id int primary key,
 products_name varchar(45),
 price int,
-zei_id int,
-constraint fk_zei
-foreign key(zei_id)
-references zei(zei_id)
+zei_id int
 );
 
 create table zei(
@@ -38,3 +29,4 @@ zei_id int primary key,
 zei_name varchar(45),
 zei_persent decimal
 );
+
--- db_task_insert.sql
+++ db_task_insert.sql
@@ -1,1 +1,15 @@
-insert into customer values(1,);
+insert into customer values
+(1,001,'沖縄県'),(2,002,'東京都'),(3,001,'北海道');
+
+insert into sale values
+(1,1),(2,2),(3,1);
+
+insert into sale_detail values
+(1,1,1,4),(2,2,2,3),(3,3,2,1);
+
+insert into products values
+(1,'天然水',150,1),(2,'おにぎり',120,1),(3,'ワックス',500,2);
+
+insert into zei values
+(1,'食料品',10),(2,'それ以外',8);
+
